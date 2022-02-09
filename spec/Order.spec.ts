04d027e2c6d0679caac400e38595d869b2b01d4c
readm1 <M.Read1@elsevier.com>
2022-02-09 13:09:13 +0000

    passing first orderInit test

--- spec/Order.spec.ts
+++ spec/Order.spec.ts
@@ -1,6 +1,12 @@
 import { Order } from "../src/Order";
-import { Crust } from "./Crust";
+// import { Crust } from "./Crust";
 import { Pizza } from "../src/Pizza";
-import { Topping } from "./Topping";
+// import { Topping } from "./Topping";
 
 const orderInit = new Order();
+
+describe("Order", function () {
+  it("default pizza is Pizza objects", function () {
+    return expect(orderInit.pizza).toEqual(new Pizza());
+  });
+});
