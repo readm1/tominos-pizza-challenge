f9092c60b2ed35af50062cdbb713ec678ac3a313
readm1 <M.Read1@elsevier.com>
2022-02-09 12:28:32 +0000

    topping init, set & get tests

--- spec/Topping.spec.ts
+++ spec/Topping.spec.ts
@@ -1,11 +1,30 @@
 import { Topping } from "../src/Topping";
+
+const toppingInit = new Topping();
+const toppingSet = new Topping();
+const toppingGet = new Topping();
+
 describe("Topping", function () {
   it("default name is blank", function () {
-    const topping = new Topping();
-    expect(topping.name).toBe("");
+    expect(toppingInit.name).toBe("");
   });
   it("default price is 0", function () {
-    const topping = new Topping();
-    expect(topping.price).toBe(0);
+    expect(toppingInit.price).toBe(0);
+  });
+  it("sets the topping name", function () {
+    toppingSet.setName("Olives");
+    expect(toppingSet.name).toBe("Olives");
+  });
+  it("sets the topping price", function () {
+    toppingSet.setPrice(20);
+    expect(toppingSet.price).toBe(20);
+  });
+  it("gets the topping name", function () {
+    toppingGet.setName("Olives");
+    expect(toppingGet.getName()).toBe("Olives");
+  });
+  it("gets the topping price", function () {
+    toppingGet.setPrice(20);
+    expect(toppingGet.getPrice()).toBe(20);
   });
 });
