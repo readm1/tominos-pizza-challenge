ca4ea41257cb2b380abb0b2bd9a8894839e65cce
readm1 <M.Read1@elsevier.com>
2022-02-09 13:16:44 +0000

    passing all orderInit tests

--- spec/Order.spec.ts
+++ spec/Order.spec.ts
@@ -1,12 +1,18 @@
 import { Order } from "../src/Order";
-// import { Crust } from "./Crust";
+import { Crust } from "../src/Crust";
 import { Pizza } from "../src/Pizza";
-// import { Topping } from "./Topping";
+import { Topping } from "../src/Topping";
 
 const orderInit = new Order();
 
 describe("Order", function () {
-  it("default pizza is Pizza objects", function () {
+  it("default pizza is Pizza object", function () {
     return expect(orderInit.pizza).toEqual(new Pizza());
+  });
+  it("default crust is Crust object", function () {
+    return expect(orderInit.crust).toEqual(new Crust());
+  });
+  it("default toppings is empty array", function () {
+    return expect(orderInit.toppings).toEqual([]);
   });
 });
