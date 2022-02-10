9187c9c996ef51a44cafbfa7f5669bfa2214fe9a
readm1 <M.Read1@elsevier.com>
2022-02-10 11:51:21 +0000

    setting up front end

---
+++ script.js
@@ -0,0 +1,30 @@
+"use strict";
+
+const pizza = {
+  margherita: ["Margherita", "Simple Veg", 2],
+  cheeseNTomato: ["Cheese 'n' Tomato", "Simple Veg", 2],
+  farmhouse: ["Farmhouse", "Classic Veg", 2.5],
+  vegSupreme: ["Veg Supreme", "Classic Veg", 2.5],
+  mexicanGreenWave: ["Mexican Green Wave", "Exotic Veg", 3],
+  peppyPaneer: ["Peppy Paneer", "Exotic Veg", 3],
+};
+
+const crust = {
+  pan: ["Pan", 0],
+  thin: ["Thin", 40],
+  cheeseBurst: ["Cheese Burst", 80],
+};
+
+const topping = {
+  tomato: ["Tomato", 10],
+  jalapeno: ["Jalapeno", 10],
+  olives: ["Olives", 20],
+  paneer: ["Paneer", 20],
+  capsicum: ["Capsicum", 10],
+  corn: ["Corn", 10],
+  cheese: ["Cheese", 30],
+};
+
+// Creating DOM Elements
+
+// const displayPizza = function ()
