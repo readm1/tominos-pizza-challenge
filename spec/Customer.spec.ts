e6cf4037d5f5ebf4d761a4348cb4e8e7c5f751d7
readm1 <M.Read1@elsevier.com>
2022-02-11 10:37:53 +0000

    written failing .corporate test (.corporate doesn't exist)

--- spec/Customer.spec.ts
+++ spec/Customer.spec.ts
@@ -6,5 +6,17 @@ import { Basket } from "../src/Basket";
 import { Customer } from "../src/Customer";
 
 describe("Customer", function () {
-  it("", function () {});
+  it("pays full price if corporate === false", function () {
+    const brian = new Customer();
+    brian.corporate = false;
+    const brianOrder = new Order();
+    const brianBasket = new Basket();
+    const margherita = new Pizza();
+    margherita.setName("Margherita");
+    margherita.setCategory("Simple Veg");
+    margherita.setPrice(2);
+    brianOrder.addPizza(margherita);
+    brianBasket.addToBasket(brianOrder);
+    expect(brianBasket.calculatePrice()).toBe(2.0);
+  });
 });
