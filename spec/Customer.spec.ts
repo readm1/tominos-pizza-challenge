ebd38290d6428517ea104c11ff10a8da41865de8
readm1 <M.Read1@elsevier.com>
2022-02-11 10:29:48 +0000

    user story 3 branch init

---
+++ spec/Customer.spec.ts
@@ -0,0 +1,10 @@
+import { Crust } from "../src/Crust";
+import { Pizza } from "../src/Pizza";
+import { Topping } from "../src/Topping";
+import { Order } from "../src/Order";
+import { Basket } from "../src/Basket";
+import { Customer } from "../src/Customer";
+
+describe("Customer", function () {
+  it("", function () {});
+});
---
+++ src/Customer.ts
@@ -0,0 +1,12 @@
+import { Crust } from "./Crust";
+import { Pizza } from "./Pizza";
+import { Topping } from "./Topping";
+import { Order } from "./Order";
+import { Basket } from "./Basket";
+
+export class Customer {
+  //   orders: Order[];
+  constructor() {
+    // this.orders = [];
+  }
+}
